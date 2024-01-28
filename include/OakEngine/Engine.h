60c93ce9a465ac452714dc7a3124b2f5b365322b
Filip Dabkowski <filip.dabkowski@gmail.com>
2024-01-28 02:18:42 +0100

    initial commit

---
+++ include/OakEngine/Engine.h
@@ -0,0 +1,8 @@
+/**
+ * Created by Filip Dabkowski on 28/01/2024.
+ */
+
+#ifndef OAKENGINE_ENGINE_H
+#define OAKENGINE_ENGINE_H
+
+#endif //OAKENGINE_ENGINE_H
---
+++ src/Engine.cpp
@@ -0,0 +1,7 @@
+/**
+ * @file Engine.cpp
+ * @brief [Description of file]
+ * 
+ * @author Filip Dabkowski
+ * @date 28/01/2024
+ */
