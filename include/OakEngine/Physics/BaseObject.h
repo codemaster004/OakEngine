b1b9069f912c8c176bd303934db2b4b78d08cfad
Filip Dabkowski <filip.dabkowski@gmail.com>
2024-01-28 10:58:57 +0100

    Added BaseObject to OakEngine's Physics module
    A new BaseObject struct was added to the Physics namespace of OakEngine. The addition defines a general layout for game objects in terms of transformations, physics components such as collider, velocity, force, and mass.

---
+++ include/OakEngine/Physics/BaseObject.h
@@ -0,0 +1,36 @@
+/**
+ * Created by Filip Dabkowski on 28/01/2024.
+ */
+
+#ifndef OAKENGINE_BASEOBJECT_H
+#define OAKENGINE_BASEOBJECT_H
+
+#include "OakEngine/Math/Vec2.h"
+#include "OakEngine/Physics/Colliders/BaseCollider.h"
+
+
+namespace oak::Physics {
+	using namespace Math;
+	using namespace Collision;
+
+	/// Describes an object location
+	struct Transform {
+		Vec2 position;
+		Vec2 scale;
+	};
+
+	/// Base Game Object
+	struct BaseObject {
+		Transform* transform;
+		Collider* collider;
+
+		Vec2 velocity{};
+		Vec2 force{};
+		float mass = 0;
+	};
+
+	using Object = BaseObject;
+
+}
+
+#endif //OAKENGINE_BASEOBJECT_H
