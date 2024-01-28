60c97ad803e96f4e0351491ccce01fcae44bbdfd
Filip Dabkowski <filip.dabkowski@gmail.com>
2024-01-28 02:51:10 +0100

    Integrate SDL2 library into OakEngine
    The Simple DirectMedia Layer 2 (SDL2) library has been integrated into the OakEngine. This involved including necessary SDL header files, making necessary modifications to the build script and engine source files, and writing the logic for game lifecycle methods. The changes enable the game engine to handle user inputs, render frames, manage game mechanics and carry out other engine-related tasks.

--- include/OakEngine/Engine.h
+++ include/OakEngine/Engine.h
@@ -5,4 +5,112 @@
 #ifndef OAKENGINE_ENGINE_H
 #define OAKENGINE_ENGINE_H
 
+#include "SDL.h"
+
+namespace oak {
+	class Engine {
+	protected:
+		/* Private Fields */
+
+		SDL_Window* window = nullptr; ///< Application window for showing content
+		SDL_Event event{}; ///< Event that occurred by pressing or lifting some key
+
+		bool isRunning = true; ///< For main game loop.
+
+
+		/* Methods */
+
+		/**
+		 * @brief Handles the user events.
+		 *
+		 * Overridable method meant for handling of user input, meaning keyboard mouse events.
+		 *
+		 * @note By default it handles only case of system Quit and game end by pressing Esc key
+		 */
+		virtual void handleInputs();
+
+		/**
+		 * @brief Updates the game.
+		 *
+		 * This method is responsible for updating the game's state.
+		 */
+		virtual void stepObjects();
+
+		/**
+		 * @brief Renders a frame of the game.
+		 *
+		 * This method is responsible for clearing the renderer, presenting everything on the screen.
+		 */
+		virtual void renderFrame();
+
+		/**
+		 * @brief Handle in game mechanics.
+		 *
+		 * This method is meant for detecting and handling in game logic and mechanics.
+		 * For example physics, collision, npc.
+		 */
+		virtual void gameMechanics();
+
+		/**
+		 * @brief Runs the game loop.
+		 *
+		 * This function runs the game loop, handling events, updating the game state, and rendering
+		 * the frame. The loop continues until the game is no longer running.
+		 */
+		void gameTick();
+
+		/**
+	* @brief Clean up the resources used by the game.
+	*
+	* This function destroys the SDL window and renderer, and quits the SDL library.
+	*/
+		void clean();
+
+	public:
+		/* Public Fields */
+
+		static SDL_Renderer* renderer; ///< Main renderer for showing on screen.
+
+		static float delta;
+
+
+		/* Constructors & Destructors */
+
+		Engine() {
+			initialize("Powered by Oak", 640, 480);
+		};
+
+
+		Engine(int width, int height) {
+			initialize("Powered by Oak", width, height);
+		}
+
+
+		Engine(const char* title, int width, int height) {
+			initialize(title, width, height);
+		}
+
+
+		~Engine() {
+			clean();
+		}
+
+		/* Public Methods */
+
+		/**
+	 * @brief Initializes the Game object.
+	 *
+	 * This method initializes the SDL library, creates a window and a renderer, and sets the initial render draw color.
+	 *
+	 * @param title The title of the window.
+	 * @param width The width of the window.
+	 * @param height The height of the window.
+	 * @return True if the initialization is successful, otherwise false.
+	 */
+		bool initialize(const char* title, int width, int height);
+
+		virtual void run();
+
+	};
+}
 #endif //OAKENGINE_ENGINE_H
--- src/Engine.cpp
+++ src/Engine.cpp
@@ -5,3 +5,101 @@
  * @author Filip Dabkowski
  * @date 28/01/2024
  */
+
+#include "OakEngine/Engine.h"
+
+using namespace oak;
+
+
+SDL_Renderer* Engine::renderer = nullptr;
+
+float Engine::delta = 1.0 / 60;
+
+
+void Engine::run() {
+	while (isRunning) {
+		gameTick();
+		SDL_Delay((int) (Engine::delta * 1e3));
+	}
+}
+
+
+void Engine::gameMechanics() {
+
+}
+
+
+void Engine::gameTick() {
+	handleInputs();
+	gameMechanics();
+	stepObjects();
+	renderFrame();
+}
+
+
+void Engine::handleInputs() {
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+			// handle system QUIT case
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			case SDL_KEYDOWN: // Additional case to QUIT on Esc key
+				if (event.key.keysym.sym == SDLK_ESCAPE) {
+					isRunning = false;
+				}
+				break;
+		}
+	}
+}
+
+
+void Engine::stepObjects() {
+
+}
+
+
+void Engine::renderFrame() {
+	// Clear objects in renderer on graphics card
+	SDL_RenderClear(renderer);
+
+	// Show on screen everything that is inside the current renderer;
+	SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+	SDL_RenderPresent(renderer);
+}
+
+
+bool Engine::initialize(const char* title, int width, int height) {
+	// If SDL did not manage to set itself up.
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		printf("Initialization Error accrued!");
+		return false;
+	}
+
+	// Try creating window.
+	window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height,
+							  SDL_WINDOW_RESIZABLE);
+	if (window == nullptr) {
+		printf("Could not create Renderer!");
+		return false;
+	}
+
+	// Try creating renderer for drawing on the actual window.
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (renderer == nullptr) {
+		printf("Could not create Renderer!");
+		return false;
+	}
+
+	// Set the default drawing color for renderer.
+	SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+
+	return true;
+}
+
+
+void Engine::clean() {
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+}
